4f141ee68579ebe50261613094552700e6173745
Philip Nguyen <100167552+Nguyen-Philip@users.noreply.github.com>
2023-09-23 13:01:27 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser')
+var axios = require('axios').default
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+app.use('/static', express.static('public'));
+
+app.set('view engine', 'ejs')
+
+let num =Math.floor(Math.random()*2832)+1;
+let text1='https://xkcd.com/'
+let text2=num.toString();
+let text3='/info.0.json'
+let result1=text1.concat("",text2);
+let result2=result1.concat("",text3);
+
+app.get('/', function(req, res){
+    axios.get(result2).then(function(response){
+        res.render('home.ejs', {name: null, xkcdData: response.data})
+    })
+})
+
+app.post('/create', (req, res) => {
+    console.log(req.body)
+    res.redirect('/')
+})
+
+app.listen(3000, () => {
+    console.log('Started on port 3000')
+})
